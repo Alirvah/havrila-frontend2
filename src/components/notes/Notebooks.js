46956712c74a12fab04509fe3748cbe81fbda90e
Patrik <havrilap@gmail.com>
2020-09-19 00:04:45 +0200

    master: Added

--- src/components/notes/Notebooks.js
+++ src/components/notes/Notebooks.js
@@ -2,24 +2,20 @@ import { API, HOST, NOTE_URL } from "../../config/constants";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import Paper from "@material-ui/core/Paper";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
-import TextField from "@material-ui/core/TextField";
 import axios from "axios";
 
 const Notebooks = () => {
   const notebooks = useSelector((store) => store.notebooks);
   const active_notebook = useSelector((store) => store.active_notebook);
+  const notes = useSelector((store) => store.notes);
+  const active_note = useSelector((store) => store.active_note);
+
   const refresh = useSelector((store) => store.refresh);
+
   const [loading, setLoading] = useState(true);
-  const [open, setOpen] = useState(false);
-  const [newNotebook, setNewNotebook] = useState("");
 
   const dispatch = useDispatch();
 
@@ -46,12 +42,12 @@ const Notebooks = () => {
   }, [refresh]);
 
   const addNotebook = () => {
-    if (newNotebook) {
+    const title = prompt("Note:");
+    if (title) {
       axios
-        .post(NOTE_URL + API.NOTEBOOK, { title: newNotebook })
+        .post(NOTE_URL + API.NOTEBOOK, { title: title })
         .then((r) => {
           dispatch({ type: "SET_REFRESH" });
-          handleClose();
         })
         .catch((e) => alert(e));
     }
@@ -77,61 +73,25 @@ const Notebooks = () => {
         })
         .catch((e) => alert(e));
     } else {
-      setOpen(true);
+      addNotebook();
     }
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-    setNewNotebook("");
   };
 
   if (loading) return <div>Loading...</div>;
 
   return (
-    <>
-      <Paper square>
-        <Tabs
-          value={active_notebook}
-          indicatorColor="primary"
-          textColor="primary"
-          onChange={handleChange}
-          aria-label="disabled tabs example"
-        >
-          {notebooks &&
-            notebooks.map((e) => <Tab key={e.id} label={e.title} />)}
-          <Tab label="+" />
-        </Tabs>
-      </Paper>
-
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
+    <Paper square>
+      <Tabs
+        value={active_notebook}
+        indicatorColor="primary"
+        textColor="primary"
+        onChange={handleChange}
+        aria-label="disabled tabs example"
       >
-        <DialogTitle id="form-dialog-title">Notebook</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Notebook name"
-            type="text"
-            fullWidth
-            onChange={(e) => setNewNotebook(e.target.value)}
-            value={newNotebook}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={addNotebook} color="primary">
-            Create
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+        {notebooks && notebooks.map((e) => <Tab key={e.id} label={e.title} />)}
+        <Tab label="+" />
+      </Tabs>
+    </Paper>
   );
 };
 
