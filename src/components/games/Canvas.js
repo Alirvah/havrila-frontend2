e5ed051215b513c22572e1328e363f2364bf931d
Patrik Havrila <havrilap@gmail.com>
2022-03-09 19:19:47 +0100

    motion sensor

--- src/components/games/Canvas.js
+++ src/components/games/Canvas.js
@@ -1,5 +1,5 @@
 import { Button } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const style = {
@@ -8,6 +8,22 @@ const style = {
 };
 
 export default function Canvas() {
+  const [test, setTest] = useState(null);
+  function motion(event) {
+    setTest(
+      "Accelerometer: " +
+        event.accelerationIncludingGravity.x +
+        ", " +
+        event.accelerationIncludingGravity.y +
+        ", " +
+        event.accelerationIncludingGravity.z
+    );
+  }
+  if (window.DeviceMotionEvent) {
+    window.addEventListener("devicemotion", motion, false);
+  } else {
+    console.log("DeviceMotionEvent is not supported");
+  }
   return (
     <>
       <Link to="/canvas/balls">
@@ -20,6 +36,7 @@ export default function Canvas() {
           Planets
         </Button>
       </Link>
+      <p>{test}</p>
       <br />
     </>
   );
