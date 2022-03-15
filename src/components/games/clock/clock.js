87a7b0ebc6caa220e076c9232b29ea9cef4f8c80
Patrik Havrila <havrilap@gmail.com>
2022-03-15 17:34:27 +0100

    design

--- src/components/games/clock/clock.js
+++ src/components/games/clock/clock.js
@@ -5,7 +5,7 @@ let fps = 60;
 let c;
 
 const Clock = class {
-  constructor(x, y, s) {
+  constructor(x, y, s, stopWatch) {
     this.x = x;
     this.y = y;
     this.s = s;
@@ -16,20 +16,10 @@ const Clock = class {
     this.milis = 0;
     this.day = 0;
     this.smooth = false;
+    this.stopWatch = stopWatch || false;
   }
   update() {
-    //this.milis += 999 / 60;
-    //if (this.milis % this.milis === 0) this.sec += 1;
-    //const time = new Date(
-    //  2018,
-    //  11,
-    //  24,
-    //  this.hour,
-    //  this.min,
-    //  this.sec,
-    //  this.milis
-    //);
-    //const time = new Date(2018, 11, 24, 11, 59, 59, 0);
+    // time = new Date(2018, 11, 24, 11, 59, 59, 0);
     const time = new Date();
     this.milis = this.smooth ? time.getMilliseconds() : 0;
     this.sec = time.getSeconds();
@@ -37,6 +27,43 @@ const Clock = class {
     this.hour = time.getHours() % 12 || 12;
     this.day = time.getDate();
   }
+  hand(type, len, width, color, angle, over) {
+    let handAngle;
+    ctx.lineCap = "round";
+    if (type === "hour") {
+      handAngle = this.smooth
+        ? this.angle * this.hour * 5 +
+          5 * ((this.min * this.angle) / 60 + angle) +
+          (this.sec * this.angle) / 60 / 12
+        : this.angle * this.hour * 5 +
+          5 * ((this.min * this.angle) / 60 + angle);
+    }
+    if (type === "minute") {
+      handAngle = this.smooth
+        ? this.angle * this.min + (this.sec * this.angle) / 60 + angle
+        : this.angle * this.min + angle;
+    }
+    if (type === "second") {
+      handAngle =
+        this.angle * this.sec + (this.milis * this.angle) / 999 + angle;
+    }
+    const rot = rotateAround(
+      this.x,
+      this.y,
+      this.x,
+      this.y - this.s / len,
+      handAngle
+    );
+    ctx.beginPath();
+    ctx.lineWidth = this.s / width;
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(rot.x, rot.y);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+
+    if (over) this.hand(type, len / 0.15, width, color, 180, false);
+  }
+
   draw() {
     ctx.fillStyle = "white";
     ctx.beginPath();
@@ -45,159 +72,46 @@ const Clock = class {
     ctx.strokeStyle = "#1b5e20";
     ctx.stroke();
 
-    let rot;
     for (let i = 0; i < 360; i = i + 6) {
-      rot = rotateAround(this.x, this.y, this.x, this.y - this.s, i);
+      //points
+      const rot = rotateAround(this.x, this.y, this.x, this.y - this.s, i);
       ctx.beginPath();
       let pointSize = this.s / 100;
       if (i % 30 === 0) {
         pointSize = this.s / 40;
       }
       ctx.arc(rot.x, rot.y, pointSize, 0, Math.PI * 2, true);
+      ctx.fillStyle = "white";
       ctx.fill();
-      if (i === 0) {
-        rot = rotateAround(
-          this.x,
-          this.y,
-          this.x - 23,
-          this.y - this.s + 50,
-          i
-        );
-        ctx.font = "40px Arial";
-        ctx.fillText("12", rot.x, rot.y);
-      }
-      if (i === i * 90) {
-        rot = rotateAround(
-          this.x,
-          this.y,
-          this.x + 13,
-          this.y - this.s + 40,
-          i - 90
-        );
-        ctx.font = "40px Arial";
-        ctx.fillText("3", rot.x, rot.y);
-      }
-      if (i === i * 2 * 90) {
-        rot = rotateAround(
-          this.x,
-          this.y,
-          this.x + 12,
-          this.y - this.s + 20,
-          i - 2 * 90
-        );
-        ctx.font = "40px Arial";
-        ctx.fillText("6", rot.x, rot.y);
-      }
-      if (i === i * 270) {
-        rot = rotateAround(
-          this.x,
-          this.y,
-          this.x - 13,
-          this.y - this.s + 20,
-          i - 270
-        );
-        ctx.font = "40px Arial";
-        ctx.fillText("9", rot.x, rot.y);
+
+      // numbers
+      for (let k = 1; k <= 24; k++) {
+        if (i === i * k * 30) {
+          const fontSize = k <= 12 ? this.s / 6 : this.s / 10;
+          const rot = rotateAround(
+            this.x,
+            this.y,
+            this.x,
+            k <= 12
+              ? this.y - this.s + fontSize / 1.1
+              : this.y - this.s + fontSize / 0.3,
+            i - k * 30
+          );
+          ctx.font = fontSize + "px Arial";
+          ctx.textBaseline = "middle";
+          ctx.textAlign = "center";
+          ctx.fillStyle = k <= 12 ? "white" : "#1b5e20";
+          ctx.fillText(k.toString(), rot.x, rot.y);
+        }
       }
     }
 
-    ctx.lineCap = "round";
+    //hands
+    this.hand("hour", 1.8, 20, "white", 0, false);
+    this.hand("minute", 1.3, 45, "white", 0, false);
+    this.hand("second", 1.1, 100, "red", 0, true);
 
-    rot = rotateAround(
-      this.x,
-      this.y,
-      this.x,
-      this.y - this.s / 1.7,
-      this.smooth
-        ? this.angle * this.hour * 5 +
-            5 * ((this.min * this.angle) / 60) +
-            (this.sec * this.angle) / 60 / 12
-        : this.angle * this.hour * 5 + 5 * ((this.min * this.angle) / 60)
-    );
-    ctx.beginPath();
-    ctx.lineWidth = this.s / 18;
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(rot.x, rot.y);
-    ctx.strokeStyle = "white";
-    ctx.stroke();
-
-    rot = rotateAround(
-      this.x,
-      this.y,
-      this.x,
-      this.y - this.s / 15,
-      this.smooth
-        ? this.angle * this.hour * 5 +
-            5 * ((this.min * this.angle) / 60 + 180) +
-            (this.sec * this.angle) / 60 / 12
-        : this.angle * this.hour * 5 + 5 * ((this.min * this.angle) / 60 + 180)
-    );
-    ctx.beginPath();
-    ctx.lineWidth = this.s / 18;
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(rot.x, rot.y);
-    ctx.strokeStyle = "white";
-    ctx.stroke();
-
-    rot = rotateAround(
-      this.x,
-      this.y,
-      this.x,
-      this.y - this.s / 1.1,
-      this.smooth
-        ? this.angle * this.min + (this.sec * this.angle) / 60
-        : this.angle * this.min
-    );
-    ctx.beginPath();
-    ctx.lineWidth = this.s / 45;
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(rot.x, rot.y);
-    ctx.strokeStyle = "white";
-    ctx.stroke();
-
-    rot = rotateAround(
-      this.x,
-      this.y,
-      this.x,
-      this.y - this.s / 15,
-      this.smooth
-        ? this.angle * this.min + (this.sec * this.angle) / 60 + 180
-        : this.angle * this.min + 180
-    );
-    ctx.beginPath();
-    ctx.lineWidth = this.s / 45;
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(rot.x, rot.y);
-    ctx.strokeStyle = "white";
-    ctx.stroke();
-
-    rot = rotateAround(
-      this.x,
-      this.y,
-      this.x,
-      this.y - this.s / 1.1,
-      this.angle * this.sec + (this.milis * this.angle) / 999
-    );
-    ctx.beginPath();
-    ctx.lineWidth = this.s / 100;
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(rot.x, rot.y);
-    ctx.strokeStyle = "red";
-    ctx.stroke();
-    rot = rotateAround(
-      this.x,
-      this.y,
-      this.x,
-      this.y - this.s / 7,
-      this.angle * this.sec + (this.milis * this.angle) / 999 + 180
-    );
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(rot.x, rot.y);
-    ctx.strokeStyle = "red";
-    ctx.stroke();
-
+    //middle dot
     ctx.fillStyle = "black";
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.s / 30, 0, Math.PI * 2, true);
@@ -210,7 +124,12 @@ const setup = () => {
   c = new Clock(cnv.width / 2, cnv.height / 2, min / 3);
   const canvas = document.querySelector("canvas");
   canvas.addEventListener("mousedown", (e) => {
-    c.smooth = !c.smooth;
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+    if (mouseX && mouseY) {
+      c.smooth = !c.smooth;
+    }
   });
 };
 
