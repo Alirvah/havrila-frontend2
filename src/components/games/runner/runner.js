b2f16acfa401aaeed50aead20c043375742f857a
Patrik Havrila <havrilap@gmail.com>
2022-04-06 23:31:05 +0200

    explosions

--- src/components/games/runner/runner.js
+++ src/components/games/runner/runner.js
@@ -9,8 +9,80 @@ let SPEED = 5;
 let player;
 let plattforms = [];
 let backgrounds = [];
-let img = new Image();
+let explosions = [];
+let stars = [];
+let megamanImg = new Image();
+let explosionImg = new Image();
+megamanImg.src =
+  "https://zappa-havrila-net.s3.eu-central-1.amazonaws.com/7a89e9ff-59e8-404d-9228-be5b3eabf85f.png";
+
+explosionImg.src =
+  "https://zappa-havrila-net.s3.eu-central-1.amazonaws.com/cde7bbb8-5909-461b-bed0-1579c889fa3a.png";
+
 let deltaTime = 0;
+
+const Star = class {
+  constructor(x, y, s) {
+    this.pos = new Point(x, y);
+    this.s = s;
+  }
+  update() {
+    if (player.pos.x >= cnv.width / 5) {
+      this.pos.x -= SPEED / 30;
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = cnv.width;
+    }
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, this.s, 0, Math.PI * 2, true);
+    ctx.fillStyle = "white";
+    ctx.fill();
+  }
+};
+
+const Explosion = class {
+  constructor(x, y, size, background, a) {
+    this.pos = new Point(x, y);
+    this.frame = 0;
+    this.row = 0;
+    this.size = size || Math.random() * 100 + 50;
+    this.background = background || Math.round(Math.random() * 5 + 1);
+    this.animation = a || Math.round(Math.random() * 5 + 2);
+  }
+  update() {
+    if (player.pos.x >= cnv.width / 5) {
+      this.pos.x -= SPEED / this.background;
+    }
+    if (this.pos.x + this.size < 0) {
+      this.pos.x = cnv.width + Math.random() * cnv.width;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      explosionImg,
+      this.frame * 256,
+      this.row * 256,
+      256,
+      256,
+      this.pos.x,
+      this.pos.y,
+      this.size,
+      this.size
+    );
+    if (deltaTime % this.animation === 0) {
+      this.frame += 1;
+      if (this.frame % 7 === 0) {
+        this.frame = 0;
+        this.row += 1;
+        if (this.row > 5) {
+          this.row = 0;
+        }
+      }
+    }
+  }
+};
 
 const Backround = class {
   constructor(x, y, w, h) {
@@ -75,9 +147,19 @@ const Player = class {
   collision(platform) {
     return pointInsideRectangle(
       platform.pos.x,
-      platform.pos.y - player.height,
+      platform.pos.y - this.height,
       platform.pos.x + platform.width,
       platform.pos.y + platform.height,
+      this.pos.x,
+      this.pos.y
+    );
+  }
+  collisionExplosion(explosion) {
+    return pointInsideRectangle(
+      explosion.pos.x,
+      explosion.pos.y - this.height,
+      explosion.pos.x + explosion.size,
+      explosion.pos.y + explosion.size,
       this.pos.x,
       this.pos.y
     );
@@ -108,7 +190,7 @@ const Player = class {
         }
       }
       ctx.drawImage(
-        img,
+        megamanImg,
         this.frame * 50,
         203,
         50,
@@ -120,7 +202,19 @@ const Player = class {
       );
     }
     if (this.action === "jumping") {
-      ctx.drawImage(img, 0, 405, 50, 38, this.pos.x, this.pos.y, 50, 38);
+      ctx.drawImage(megamanImg, 0, 405, 50, 38, this.pos.x, this.pos.y, 50, 38);
+
+      ctx.drawImage(
+        explosionImg,
+        this.frame * 256,
+        1 * 256,
+        256,
+        256,
+        this.pos.x,
+        this.pos.y,
+        20,
+        20
+      );
     }
   }
 };
@@ -128,16 +222,46 @@ const Player = class {
 const setup = () => {
   player = new Player(50, 50, 50, 38);
   plattforms = [
-    new Platform(0, cnv.height - 50, cnv.width / 2, 50),
-    new Platform(cnv.width / 2 + 20, cnv.height - 50, cnv.width / 2, 50),
-    new Platform(100, 158, 300, 20),
-    new Platform(100, cnv.height / 2 + 100, 4000, 20),
+    //  new Platform(0, cnv.height - 50, cnv.width / 2, 50),
+    //  new Platform(cnv.width / 2 + 20, cnv.height - 50, cnv.width / 2, 50),
+    //  new Platform(100, 158, 300, 20),
+    //  new Platform(100, cnv.height / 2 + 100, 4000, 20),
   ];
+  for (let i = 0; i < 2; i++) {
+    plattforms.push(
+      new Platform(
+        Math.random() * cnv.width,
+        Math.random() * (cnv.height / 2) + cnv.height / 2,
+        Math.random() * 1000 + 100,
+        20
+      )
+    );
+  }
+
   backgrounds = [
     new Backround(500, cnv.height / 2, 300, cnv.height),
     new Backround(cnv.width / 2, 200, 200, cnv.height),
     new Backround(800, 300, 400, cnv.height),
   ];
+  explosions = [];
+  for (let i = 0; i < 3; i++) {
+    explosions.push(
+      new Explosion(
+        Math.random() * (cnv.width / 2) + cnv.width / 2,
+        Math.random() * (cnv.height / 2) - 50
+      )
+    );
+  }
+  stars = [];
+  for (let i = 0; i < 200; i++) {
+    stars.push(
+      new Star(
+        Math.random() * cnv.width,
+        Math.random() * cnv.height,
+        Math.random()
+      )
+    );
+  }
 };
 
 const render = () => {
@@ -153,16 +277,28 @@ const render = () => {
     }
   });
 
+  explosions.forEach((explosion) => {
+    if (player.collisionExplosion(explosion)) {
+      if (explosion.row > 1 && explosion.row < 4) {
+        player.live = false;
+      }
+    }
+  });
+
   if (!player.live) {
     setup();
   }
 
+  stars.forEach((p) => p.update());
   backgrounds.forEach((p) => p.update());
   plattforms.forEach((p) => p.update());
+  explosions.forEach((e) => e.update());
   player.update();
 
+  stars.forEach((p) => p.draw());
   backgrounds.forEach((p) => p.draw());
   plattforms.forEach((p) => p.draw());
+  explosions.forEach((e) => e.draw());
   player.draw();
 };
 
@@ -173,17 +309,14 @@ const loop = (canvasName) => {
   cnv.height = Math.floor(window.innerHeight / 1.3);
   ctx.clearRect(0, 0, cnv.width, cnv.height);
 
-  img.onload = function () {
-    //ctx.drawImage(img, 50, 50);
-  };
-  img.src =
-    "https://www.nicepng.com/png/full/222-2222069_megaman-sprite-png-megaman-sprite-sheet-png.png";
-
   const canvas = document.querySelector("canvas");
   canvas.addEventListener("mousedown", (e) => {
     //const rect = canvas.getBoundingClientRect();
     //const x = e.clientX - rect.left;
     //const y = e.clientY - rect.top;
+    if (player.action !== "jumping") {
+      player.pos.y -= 1;
+    }
     player.vel.y -= 10;
     player.action = "jumping";
   });
